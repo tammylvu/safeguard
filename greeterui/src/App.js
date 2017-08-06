02efbec42419b1f15f71f58a8802488cf6827455
Collin Chin <collin.chin@berkeley.edu>
2017-08-05 21:22:50 -0700

    solutions removed

--- greeterui/src/App.js
+++ greeterui/src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            greeting: "",
+            greeting: "Hello",
             addr: addr,
             balance: balance,
             blockNum: blockNum,
--- greeterui/src/EthereumSetup.js
+++ greeterui/src/EthereumSetup.js
@@ -1,24 +1,45 @@
+// import web3: https://github.com/ethereum/wiki/wiki/JavaScript-API
 import Web3 from 'web3';
-const ETHEREUM_CLIENT = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+
+// instantiate web3
+// creates a new web3 object using the testrpc instance we are running in the background
+const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+
+// set greeter's ABI (Application Binary Interface)
 var greeterABI = [{"constant":true,"inputs":[],"name":"greet","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_greeting","type":"string"}],"name":"greeter","outputs":[],"payable":false,"type":"function"}]
-var greeterAddress = '0x1c62d1d8852dab2e273a55a2954c0037d2cace4d';
-const greeterContract = ETHEREUM_CLIENT.eth.contract(greeterABI).at(greeterAddress);
 
-//get Testrpc account 0 address (default account)
-var addr = ETHEREUM_CLIENT.eth.accounts[0];
+// this should be the address of the mined Greeter contract on testrpc after truffle migrate
+var greeterAddress = '0x9b1f7f645351af3631a656421ed2e40f2802e6c0';
 
-//get balance of account 0 in Ether
-var balanceBigNum = ETHEREUM_CLIENT.eth.getBalance(addr);
-var balanceInWei = balanceBigNum.plus(21).toString(10);
-var balance = ETHEREUM_CLIENT.fromWei(balanceInWei, 'ether');
+// creates a greeterContract variable that we can use to call Greeter.sol methods
+const greeterContract = web3.eth.contract(greeterABI).at(greeterAddress);
 
-//get block number
-var blockNum = ETHEREUM_CLIENT.eth.blockNumber;
+/* TASK 1:
+ * get block number
+ */
+var blockNum =
 
-//get hashrate
-var hashrate = ETHEREUM_CLIENT.eth.hashrate;
+/* TASK 2:
+ * get hashrate
+ */
+var hashrate =
 
-//get gas price
-var gasPrice = ETHEREUM_CLIENT.eth.gasPrice.toString(10);
+/* TASK 3:
+ * get gas price
+ */
+var gasPrice =
+
+/* TASK 4:
+ * Get the balance of your default account.
+ * You may not need all variables
+ */
+ 
+// get Testrpc account 0 address (default account)
+var addr =
+// get balance of account 0 in Ether
+// HINT: the web3 getBalance function returns a BigNumber but we want the balance in Ether
+var balanceBigNum =
+var balanceInWei =
+var balance =
 
 export {greeterContract, addr, balance, blockNum, hashrate, gasPrice};
